42e08a16be7036b06710635c9562af5cc4e67808
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-03-19 18:41:49 +0800

    Passed instance variables of artworks artwork users and user in artworks and users controllers

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,11 +1,14 @@
 class ArtworksController < ApplicationController
   def index
+    @artworks = Artwork.all
   end
 
   def show
+    @artwork = Artwork.find(params[:id])
   end
 
   def new
+    @artwork = Artwork.new
   end
 
   def create
